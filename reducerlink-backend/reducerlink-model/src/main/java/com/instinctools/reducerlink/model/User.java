d93a7c7f62cb0f4112c164fb6bae93f464d5686c
dzianis_zayerka <dzianis_zayerka@senla.eu>
2016-10-28 12:08:56 +0300

    Correct structure of project

---
+++ reducerlink-backend/reducerlink-model/src/main/java/com/instinctools/reducerlink/model/User.java
@@ -0,0 +1,8 @@
+package com.instinctools.reducerlink.model;
+
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+public class User {
+
+}
